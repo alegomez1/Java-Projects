ee262135419f4c45923318651daf8772cf8ddc18
Alex <alegomez1@me.com>
2019-04-05 15:42:54 -0400

    New code

---
+++ Playground/src/QTest.java
@@ -0,0 +1,6 @@
+public class QTest {
+
+    public static void main(String[] args) {
+        String arr[] = new String[10];
+    }
+}
--- SchoolWork/src/Lab10/Lab10.java
+++ SchoolWork/src/Lab10/Lab10.java
@@ -1,6 +1,85 @@
 package Lab10;
 import java.util.Scanner;
 
+//Task 1 Questions:
+/*
+
+	Task 1: What kind of errors are is caused by the following code, if any?
+            How would we fix them?
+            Note: Some errors may be caused before execution (before you run
+            the program). In this case, list the error written in NetBeans
+            when you hover over the problem.
+
+            //Task 1a.
+            String[] arr = new String[10];
+            arr[10] = "This is a String";
+
+            Exception: Out of bounds error
+
+            What caused the error: The fact that there is a 10 in the index of arr in the second line when that value does not exist
+
+            How to Fix It: Either change the 10 in the second like to a number from 0-9 or change the 10 in the first line to an 11
+
+            --------------------------------------------------------------------
+
+            //Task 1b.
+            String[] arr = new String[-1];
+
+            Exception: Negative array size exception
+
+            What caused the error: There being a -1 in the size of the array
+
+            How to Fix It: Change the -1 to a positive value
+
+            --------------------------------------------------------------------
+
+            //Task 1c.
+            String arr = new String[10];
+
+            Exception: Incompatible types
+
+            What caused the error: There not being a set of brackets after 'String' to the left of the declaration of the arr array
+
+            How to Fix It: Change it to look like String[] arr = new String[10];
+
+            --------------------------------------------------------------------
+
+            //Task 1d.
+            String[] arr = new String[10];
+            System.out.println(arr[2].length());
+
+            Exception: Null pointer exception
+
+            What caused the error: The fact that arr[2] does not exist yet so it's null
+
+            How to Fix It: Add a value to arr[2]
+
+            --------------------------------------------------------------------
+
+            //Task 1e.
+            Object x[] = new String[1];
+            x[0] = new Integer(0);
+
+            Exception: Array store exception
+
+            What caused the error: Trying to assign a String array an Integer value
+
+            How to Fix It: Either switch the array type to be of Integer or changed the value being stored to be of type String
+
+            --------------------------------------------------------------------
+
+            //Task 1f.
+            String arr[] = new String[10];
+
+            Exception: No error
+
+            What caused the error: N/A
+
+            How to Fix It: N/A
+
+*/
+
+
 public class Lab10 {
     //Creating global String array
     public static String[] data;
