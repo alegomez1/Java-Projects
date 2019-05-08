28c9fb8fc49f213ca65aaae0c162d9b4a2dfbd88
alejandrogomez <alegomez1@me.com>
2019-05-08 14:01:28 -0400

    test

--- TestProjects/src/Room/RoomConstructor.java
+++ TestProjects/src/Room/RoomConstructor.java
@@ -1,8 +1,18 @@
 package Room;
 
+import java.util.Scanner;
+
 public class RoomConstructor {
 
     static double totalSurfaceArea = 0;
+
+    static double oneGallonPrice = 20.00;
+    static double oneGallonCoverage = 350.00;
+
+    static double threeGallonPrice = 55.00;
+    static double threeGallonCoverage = 1050.00;
+
+
 
     //Functions used
 
@@ -10,6 +20,34 @@ public class RoomConstructor {
     public void calcSurfaceArea(double length, double height){
         double surfaceArea = length*height;
         totalSurfaceArea += surfaceArea;
+    }
+
+    //Asks for the number of walls built, then runs the calcSurfaceArea function
+    public void askForWalls() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many walls are you going to paint?\nEnter value: ");
+        int numberOfWalls = input.nextInt();
+
+        for (int i = 0; i < numberOfWalls; i++) {
+            int trueNumber = i + 1;
+            System.out.println("What is the length of wall " + trueNumber + "?");
+            double length = input.nextDouble();
+            System.out.println("What is the height of wall " + trueNumber + "?");
+            double height = input.nextDouble();
+            calcSurfaceArea(length, height);
+        }
+    }
+
+    public void gallonsNeeded(){
+
+       double gallonsNeeded = totalSurfaceArea/350.00;
+       double singleGallonPrices = gallonsNeeded*oneGallonPrice;
+       double tripleGallonPrices = gallonsNeeded*threeGallonPrice;
+        System.out.printf("You will need approximately %.2f gallons of paint\n", gallonsNeeded);
+        System.out.printf("Using single gallon buckets it will cost about $%.2f\n", singleGallonPrices);
+        System.out.printf("Using triple gallon buckets it will cost about $%.2f\n", tripleGallonPrices);
+
     }
 
     //Getters and setters
--- TestProjects/src/Room/RoomMain.java
+++ TestProjects/src/Room/RoomMain.java
@@ -7,25 +7,14 @@ public class RoomMain {
 
 
     public static void main(String[] args) {
-        askForWalls();
+
+        room.askForWalls();
         System.out.println(room.getTotalSurfaceArea());
+        room.gallonsNeeded();
+
+
 
     }
 
-    public static void askForWalls(){
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("How many walls are you going to paint?\nEnter value: ");
-        int numberOfWalls = input.nextInt();
-
-        for (int i =0; i < numberOfWalls; i++){
-            int trueNumber = i+1;
-            System.out.println("What is the length of wall " + trueNumber + "?");
-            double length = input.nextDouble();
-            System.out.println("What is the height of wall " + trueNumber + "?");
-            double height = input.nextDouble();
-            room.calcSurfaceArea(length, height);
-        }
-
-    }
 }
